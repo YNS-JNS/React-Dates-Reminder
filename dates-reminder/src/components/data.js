3c7c54b0ddb9e81d31db359bd2e6a09a877f0c0f
Youness.Aitmbarek <Youness.Aitmbarek@ITMMAAGC3XX8NN2.annuaire.agence.casa>
2023-10-19 19:26:41 +0100

    Deleted the folder dates-reminder

--- dates-reminder/src/components/data.js
+++
@@ -1,7 +0,0 @@
-export const personData = [{ id: '0', name: 'Leanne Graham', date: '09:05', img: 'person.jpg' },
-{ id: '1', name: 'Glenna Reichert', date: '10:30', img: 'hijab.jpg' },
-{ id: '2', name: 'Ahmed Farid', date: '11:45', img: 'p1.jpg' },
-{ id: '3', name: 'Clementina DuBuque', date: '15:00', img: 'woman.jpg' },
-{ id: '4', name: 'Youness Jonas', date: '16:10', img: 'p1.jpg' },
-{ id: '5', name: 'Johns Group', date: '16:50', img: 'p1.jpg' },
-{ id: '6', name: 'Nicholas Runolfsdottir', date: '20:30', img: 'p1.jpg' },];
